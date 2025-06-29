991121587085a148b784321cafc164e880bbbf95
Jackie Yao <jackieyao@futunn.com>
2025-06-29 10:59:56 +0800

    feat: add ssr-movie spider

---
+++ 2-spider/ssrSpider.py
@@ -0,0 +1,69 @@
+import requests
+import logging
+import json
+from pyquery import PyQuery as pq
+from urllib.parse import urljoin
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s:%(message)s')
+
+BASE_URL = 'https://ssr1.scrape.center'
+TOTAL_PAGE = 10
+
+def get_source_code(url):
+    logging.info('>>> get_source_code start, url:%s', url)
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            logging.info('>>> get_source_code success, url:%s', url)
+            return response.text
+    except requests.RequestException:
+        logging.error('get_source_code failed, url=%s', url, exc_info=True)
+
+def parse_list(html):
+    logging.info('>>> parse_list start...')
+
+    doc = pq(html)
+    cards = doc('.el-card')
+    cardsData = []
+
+    for card in cards.items():
+        name = card.find('.name h2').text()
+        score = card.find('.score').text()
+        detail_url = card.find('.name').attr('href')
+        logging.info('name %s, score %s', name, score)
+        cardsData.append({
+            'name': name,
+            'score': score,
+            'detail_url': f'{BASE_URL}{detail_url}'
+        })
+
+    
+    logging.info('>>> parse_list success...')
+    return cardsData
+
+def save_json(data, filename):
+    try:
+        logging.info('>>> save_json success...')
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+
+        logging.info('save_json success, filename=%s', filename)
+        return True
+    except Exception as e:
+        logging.error('save_json failed, {e}', exc_info=True)
+        return False
+            
+def main():
+    print('main function start...')
+
+    list_url = f'{BASE_URL}/page/1'
+    list_code = get_source_code(list_url)
+    list_data = parse_list(list_code)
+    save_json(list_data, 'ssr-spider.json')
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # python-start
 > Python学习记录。
 
-**示例**
+**学习记录**
 
-- [打印三角形](./1-base/triangle.py)
+- 2025-06-25: [打印三角形](./1-base/triangle.py)
 
-- [爬虫篇｜豆瓣电影Top250](./1-base/doubanTop250.py)
+- 2025-06-26: [爬虫篇｜豆瓣电影Top250](./1-base/doubanTop250.py)
 
-- [爬虫篇｜读取股票价格](./1-base/stockPrice.py)
+- 2025-06-27: [爬虫篇｜读取股票价格](./1-base/stockPrice.py)
+
+- 2025-06-29: [爬虫篇｜SSR网站电影列表](./2-spider/ssrSpider.py)
